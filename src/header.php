4d41aa64d73efd373d0baf768589e44189b156fa
gpalsingh <gurkirpal204@gmail.com>
2017-03-24 15:59:17 +0530

    Add login functionality

--- src/header.php
+++ src/header.php
@@ -1,10 +1,26 @@
+<?php
+	include_once "common.php";
+	start_new_session();
+	if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+		if (array_key_exists('logout', $_GET) && ($_GET['logout'] == true)) {
+			logoutUser();
+		}
+	}
+	if (!array_key_exists('valid', $_SESSION)) {
+		initSession();
+	}
+?>
 <div class="header">
 <link rel="stylesheet" href="css/main.css">
 <script src="js/common.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 <a href="about.php">About Us</a>
 <a href="terms.php">Terms</a>
-<input type="text" placeholder="search"> sign-in
+<input type="text" placeholder="search">
+<?php if(!$_SESSION['valid'] == true) { ?>
+<a href="signin.php">Sign-in</a>
 <a href="register.php">Register</a>
-cart
+<?php } else { ?>
+<a href="http://localhost?logout=true">Logout</a>
+<?php } ?>
 </div>
---
+++ src/signin.php
@@ -0,0 +1,70 @@
+<html><head><title>Register</title></head>
+<body>
+<?php
+include_once "common.php";
+$errors_list = Array();
+start_new_session();
+
+function isValidUser($uname, $pass) {
+	global $errors_list;
+	$conn = new mysqli("localhost", "pma", "pmapass", "yourmart");
+	if (!$conn) {
+		array_push($errors_list, "Cannot connect to database :(");
+		$conn->close();
+		return false;
+	}
+	$query_base = "SELECT * FROM user_data WHERE username='%s' AND password='%s';";
+	$query_string = sprintf($query_base, $uname, $pass);
+	$sql = $conn->query($query_string);
+	echo $conn->connect_error;
+	if (($conn->affected_rows) < 1) {
+		array_push($errors_list, "Username and/or Password is wrong.");
+		$conn->close();
+		return false;
+	}
+	$conn->close();
+	return true;
+}
+	
+if (isset($_POST['login_button']) && !empty($_POST['username']) && !empty($_POST['password'])) {
+	$username = $_POST['username'];
+	$pass = $_POST['password'];
+	if (isValidUser($username, $pass)) {
+		$_SESSION['valid'] = true;
+		$_SESSION['username'] = $username;
+	} else {
+		$_SESSION['valid'] = false;
+		array_push($errors_list, "Failed to log you in.");
+	}
+}
+?>
+
+<?php
+include_once "header.php"
+?>
+<div class="content">
+<h1>Sign-in to YourMart</h1>
+<?php
+$show_form = true;
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	if (!$_SESSION['valid']) {
+		displayFormErrors($errors_list);
+	} else {
+		$show_form = false;
+	}
+}
+?>
+<?php if ($show_form) { ?>
+<form method="POST" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>">
+Username: <input type=text name="username" placeholder="Your username" required autofocus>
+<br>
+Password: <input type=password name="password" placeholder="Your password" required>
+<br>
+<button type="submit" name="login_button">Login</button>
+</div>
+<form>
+<?php } else echo "Welcome, $username." ?>
+<?php
+include_once "footer.php"
+?>
+</body></html>
