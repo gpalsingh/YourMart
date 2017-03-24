fc6b9c2aa642a178fbc46fc7b4ce7c228de03c8a
gpalsingh <gurkirpal204@gmail.com>
2017-03-24 15:57:22 +0530

    Add common php functions

---
+++ src/common.php
@@ -0,0 +1,22 @@
+<?php
+function displayFormErrors($errors_list) {
+	foreach ($errors_list as $error_val) {
+		echo "<span class=\"form-errors\">$error_val</span>";
+	}
+}
+
+function initSession() {
+	$_SESSION['valid'] = false;
+	$_SESSION['username']= null;
+}
+
+function logoutUser() {
+	initSession();
+}
+
+function start_new_session() {
+	if (session_status() == PHP_SESSION_NONE) {
+		session_start();
+	}
+}
+?>
