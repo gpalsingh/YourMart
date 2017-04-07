bdf0f980f2ca7a346091fb24498a01c43f3e9f6e
gpalsingh <gurkirpal204@gmail.com>
2017-04-07 15:52:02 +0530

    Added github link

--- README.rst
+++ README.rst
@@ -2,3 +2,7 @@
 YourMart
 ===============
 Basic website making use of database operations like INSERT, SELECT, UPDATE, DELETE, etc.
+
+`https://github.com/gpalsingh/YourMart`_
+
+.. _`https://github.com/gpalsingh/YourMart`: https://github.com/gpalsingh/YourMart
--- src/footer.php
+++ src/footer.php
@@ -1,7 +1,9 @@
 <div class="footer">
 <link rel="stylesheet" href="css/footer.css">
 <div class="footer_text">
-	<div class="project-left">bca-iii<br>project</div>
+	<div class="project-left">bca-iii<br>project(
+	<span style="text-transform:none">
+	<a href="https://github.com/gpalsingh/YourMart">https://github.com/gpalsingh/YourMart</a></span>)</div>
 	<div class="names">gurkirpal singh<br>abhishek jindal</div>
 </div>
 </div>
